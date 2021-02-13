a41ec2a8bba9165bcecb8d6f96d03e7cdb316a2f
MohammedEssa40 <79007284+MohammedEssa40@users.noreply.github.com>
2021-02-13 16:05:17 +0400

    Error that I can’t fix
    The code “ if get_button_pos().length() > boundary:” is wrong I guess, it shows me that the code is not declared I tried typing the code again but it didn’t work please respond

--- non-shaders/Joystick_Button.gd
+++ non-shaders/Joystick_Button.gd
@@ -28,7 +28,7 @@ func _input(event):
 		if event_dist_from_centre <= boundary * global_scale.x or event.get_index() == ongoing_drag:
 			set_global_position(event.position - radius * global_scale)
 
-			if get_button_pos().length() > boundary:
+This code--->>	 	      if get_button_pos().length() > boundary:
 				set_position( get_button_pos().normalized() * boundary - radius)
 
 			ongoing_drag = event.get_index()
